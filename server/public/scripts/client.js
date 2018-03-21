79bc7c9915a3fe39b068b3be9b9ba0d73287bfb1
Blake Burbach <cras.bburbach@gmail.com>
2018-03-21 13:34:36 -0500

    Just added a few more things to client.js trying out some things with angular and the controller.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,4 +4,8 @@ myApp.controller("WelcomeController", function(){
    console.log('WelcomeController working'); 
    let self = this;
    self.disclaimer = "Be a HADAR not a Hater!";
+   self.thisIsMyName = "Call me Sparticus!";
 });
+
+console.log('AngularJS is cooooool');
+console.log('I just found out that this comes BEFORE the controller');
