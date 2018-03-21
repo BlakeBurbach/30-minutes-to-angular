5f404090770eedef424f9b8bb55359380f6e5e5d
Blake Burbach <cras.bburbach@gmail.com>
2018-03-21 13:29:39 -0500

    Got angular to interact with html and javascript and append to the DOM through the ng-controller and ng-model input.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,2 +1,7 @@
 let myApp = angular.module('myApp', []);
 
+myApp.controller("WelcomeController", function(){
+   console.log('WelcomeController working'); 
+   let self = this;
+   self.disclaimer = "Be a HADAR not a Hater!";
+});
